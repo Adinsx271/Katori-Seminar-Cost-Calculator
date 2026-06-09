237f0dc5bf87801ae90545c98da7100c6e11aeb9
Adinsx271 <79975774+Adinsx271@users.noreply.github.com>
2026-06-09 14:25:20 +0200

    Update for 2026

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let inputList = new Array(17).fill(false);
+let inputList = new Array(18).fill(false);
 let costArray = [
-    [30, 0, 25, 25],
-    [30, 0, 30, 25],
-    [30, 0, 30, 25],
-    [30, 0, 30, 25],
+    [30, 30, 25, 25],
+    [30, 30, 25, 25],
+    [30, 30, 25, 25],
+    [30, 25, 30, 25],
     [25, 30, 0, 0]
 ];
 let Costs = 0
@@ -61,10 +61,10 @@ function calculateCosts() {
     }
     let tempDesc_WEEK = ""
     stackOfTrainingsWEEK.sort((a, b) => b - a);
-    while (stackOfTrainingsWEEK.length >= 3 && arraySum(stackOfTrainingsWEEK.slice(0, 3)) >= 80) {
+    while (stackOfTrainingsWEEK.length >= 3 && arraySum(stackOfTrainingsWEEK.slice(0, 3)) >= 75) {
         stackOfTrainingsWEEK.splice(0, 3);
-        WeekCosts += 80;
-        tempDesc_WEEK += "<br><br> +80.00 € for a set of 3 trainings";
+        WeekCosts += 75;
+        tempDesc_WEEK += "<br><br> +75.00 € for a set of 3 trainings";
     }
     WeekCosts += arraySum(stackOfTrainingsWEEK);
     for (let i=0; i < stackOfTrainingsWEEK.length; i++) {
@@ -82,14 +82,14 @@ function calculateCosts() {
     }
     let tempDesc_WEEKEND = ""
     stackOfTrainingsWEEKEND.sort((a, b) => b - a);
-    while (stackOfTrainingsWEEKEND.length >= 3 && arraySum(stackOfTrainingsWEEKEND.slice(0, 3)) >= 80) {
+    while (stackOfTrainingsWEEKEND.length >= 3 && arraySum(stackOfTrainingsWEEKEND.slice(0, 3)) >= 75) {
         stackOfTrainingsWEEKEND.splice(0, 3);
-        WeekendCosts += 80;
-        tempDesc_WEEKEND += "<br><br> +80.00 € for a set of 3 trainings"
+        WeekendCosts += 75;
+        tempDesc_WEEKEND += "<br><br> +75.00 € for a set of 3 trainings"
     }
     WeekendCosts += arraySum(stackOfTrainingsWEEKEND);
     for (let i=0; i < stackOfTrainingsWEEKEND.length; i++) {
-        tempDesc_WEEKEND += "<br><br> +" + stackOfTrainingsWEEKEND[i] + ",00 € for single training"
+        tempDesc_WEEKEND += "<br><br> +" + stackOfTrainingsWEEKEND[i] + ",00 € for single training2"
     }
     //console.log(tempDesc_WEEKEND)
 
@@ -104,11 +104,15 @@ function calculateCosts() {
     }
     let tempDesc_ALL = ""
     stackOfTrainingsALL.sort((a, b) => b - a);
-    while (stackOfTrainingsALL.length >= 3 && arraySum(stackOfTrainingsALL.slice(0, 3)) >= 80) {
+    console.log(stackOfTrainingsALL)
+    while (stackOfTrainingsALL.length >= 3 && arraySum(stackOfTrainingsALL.slice(0, 3)) >= 75) {
         stackOfTrainingsALL.splice(0, 3);
-        Costs += 80;
-        tempDesc_ALL += "<br><br> +80.00 € for a set of 3 trainings"
+        Costs += 75;
+        console.log("test")
+        tempDesc_ALL += "<br><br> +75.00 € for a set of 3 trainings"
     }
+    console.log(stackOfTrainingsALL)
+
     Costs += arraySum(stackOfTrainingsALL);
     for (let i=0; i < stackOfTrainingsALL.length; i++) {
         tempDesc_ALL += "<br><br> +" + stackOfTrainingsALL[i] + ",00 € for single training"
@@ -129,7 +133,7 @@ function calculateCosts() {
 
     let tempCosts = WeekCosts + WeekendCosts;
 
-    if (Costs >= tempCosts) {
+    if (Costs > tempCosts) {
         Costs = tempCosts;
         description = tempDesc_WEEK + tempDesc_WEEKEND
     } else {
@@ -137,34 +141,37 @@ function calculateCosts() {
         description = tempDesc_ALL
     }
 
-    if (Costs >= maxSeminarCosts) {
+    if (Costs > maxSeminarCosts) {
         Costs = maxSeminarCosts;
         flatRateSeminar = true;
+
+/*         if (inputArray[2][3]) {
+        Costs = Costs + 25; // 5. Dan Kodansha Training
+        description = "<br><br>-Single trainings Cost exceeds flat rate for whole seminar <br> Cost for whole seminar is: " + Costs + ",00 € <br> +25,00 € for 5. Dan Kodansha training";    
+        } */
+
+
         if (Costs != 0) {
             description = "<br><br>-Single trainings Cost exceeds flat rate for whole seminar <br> Cost for whole seminar is: " + Costs + ",00 €"
         }
     }
 
-    if (inputArray[3][1]) { 
+// -> THIS YEAR NOT NECESSARY
+    if (inputArray[2][3] && flatRateSeminar) { // 5. Dan Kodansha Training
         Costs += 25; 
         description += "<br><br> -Additional 25,00 € for 5. dan kodansha training"
-    }
+    } 
 
 
-    //Catch special case if only 5.Dan Kodansha + 2x 30€ Trainings 
+/*     //Catch special case if only 5.Dan Kodansha + 2x 30€ Trainings -> THIS YEAR NOT NECESSARY
     if (Costs === 85 && inputArray[3][1]) {
         Costs = 80;
-    }
+    } */
 
     if (flag_discount) {
         let discount = roundTo(Costs*0.15,2)
         Costs = Costs - discount
-        description += "<br><br> -You get a discount of 15 % on the total seminar cost (except Samurai Museum)<br> this amounts to: -" + discount.toFixed(2) + " €" 
-    }
-
-    if (inputList[16]) { 
-        Costs += 19; 
-        description += "<br><br> -Additional 19,00 € for visit of the Samurai Museum"
+        description += "<br><br> -You get a discount of 15 % on the total seminar cost <br> this amounts to: -" + discount.toFixed(2) + " €" 
     }
 
     displayCosts(Costs, WeekendCosts);
@@ -172,33 +179,41 @@ function calculateCosts() {
 
 
 
-/*     //Debug to console...
+    //Debug to console...
     console.log("Flatrate Weekend: " + flatRateWE 
         + "\n" + "Flatrate Seminar: " + flatRateSeminar 
         + "\n" + "whole Week calculated together: " + flagWeek_WE_combined
-        + "\n" + "Kodansha Training: " + inputArray[3][1]
-        + "\n" + "Samurai Museum: " + inputList[16]
-    ); */
+        + "\n" + "Kodansha Training: " + inputArray[2][3]
+    );
 }
+
+//----------------------------------------------------------------------------------------------
 
 function calculateMaxCosts() {
     let maxSeminarCosts = 300;
     let maxWeekendCosts = 120;
 
-    if (!inputArray[4][0]) { maxSeminarCosts = 285; }
-    if (!inputArray[4][0] && !inputArray[1][3] && !inputArray[3][3]) { maxSeminarCosts = 255; }
-    if (!inputArray[4][0] && !inputArray[1][3] && !inputArray[3][3] && !inputArray[0][3] && !inputArray[2][3]) { maxSeminarCosts = 215; }
-    if (!inputArray[4][0] && !inputArray[1][3] && !inputArray[3][3] && !inputArray[0][3] && !inputArray[2][3] && !inputArray[0][2]) { maxSeminarCosts = 200; }
-    if (!inputArray[4][0] && !inputArray[1][3] && !inputArray[3][3] && !inputArray[0][3] && !inputArray[2][3] && !inputArray[0][2] && !inputArray[1][2] && !inputArray[2][2] && !inputArray[3][2]) { maxSeminarCosts = 125; }
+    if (!inputArray[2][3] && !inputArray[3][1]) { maxSeminarCosts = 290; } // all but 4. Dan
+    if (!inputArray[2][3] && !inputArray[3][1] && !inputArray[1][3] && !inputArray[4][0]) { maxSeminarCosts = 260; } // all but 3. Dan
+    if (!inputArray[2][3] && !inputArray[3][1] && !inputArray[1][3] && !inputArray[4][0] && !inputArray[1][2]) { maxSeminarCosts = 245; } // all but 2. Dan
+    if (!inputArray[2][3] && !inputArray[3][1] && !inputArray[1][3] && !inputArray[4][0] && !inputArray[1][2] && !inputArray[0][3] && !inputArray[3][3]) { maxSeminarCosts = 215; } // all but 1. Dan
+    if (!inputArray[2][3] && !inputArray[3][1] && !inputArray[1][3] && !inputArray[4][0] && !inputArray[1][2] && !inputArray[0][3] && !inputArray[3][3] && !inputArray[0][1] && !inputArray[2][2]) { maxSeminarCosts = 185; } // all but 1.Kyu
+    if (!inputArray[2][3] && !inputArray[3][1] && !inputArray[1][3] && !inputArray[4][0] && !inputArray[1][2] && !inputArray[0][3] && !inputArray[3][3] && !inputArray[0][1] && !inputArray[2][2] && !inputArray[3][2]) { maxSeminarCosts = 170; } // all but 2.Kyu
+    if (!inputArray[2][3] && !inputArray[3][1] && !inputArray[1][3] && !inputArray[4][0] && !inputArray[1][2] && !inputArray[0][3] && !inputArray[3][3] && !inputArray[0][1] && !inputArray[2][2] && !inputArray[3][2] && !inputArray[1][1] && !inputArray[2][1]) { maxSeminarCosts = 140; } // all but 3.Kyu
+    if (!inputArray[2][3] && !inputArray[3][1] && !inputArray[1][3] && !inputArray[4][0] && !inputArray[1][2] && !inputArray[0][3] && !inputArray[3][3] && !inputArray[0][1] && !inputArray[2][2] && !inputArray[3][2] && !inputArray[1][1] && !inputArray[2][1] && !inputArray[0][2]) { maxSeminarCosts = 120; } // all but 4.Kyu
+
     if (matrixSum(inputArray) === 0) { maxSeminarCosts = 0; }
 
-    if (!inputArray[4][0]) { maxWeekendCosts = 100; }
-    if (!inputArray[4][0] && !inputArray[3][3]) { maxWeekendCosts = 80; }
-    if (!inputArray[4][0] && !inputArray[3][3] && !inputArray[3][2]) { maxWeekendCosts = 55; }
+    if (!inputArray[3][1]) { maxWeekendCosts = 105; } // all but 4.Dan
+    if (!inputArray[3][1] && !inputArray[4][0]) { maxWeekendCosts = 90; } // all but 3. Dan
+    if (!inputArray[3][1] && !inputArray[4][0] && !inputArray[3][3]) { maxWeekendCosts = 75; } // all but 1.Dan
+    if (!inputArray[3][1] && !inputArray[4][0] && !inputArray[3][3] && !inputArray[3][2]) { maxWeekendCosts = 55; } // all but 2.Kyu
     if (matrixSum(inputArray.slice(3, 5)) === 0) { maxWeekendCosts = 0; }
 
     return [maxSeminarCosts, maxWeekendCosts];
 }
+
+
 
 function displayCosts(SeminarCosts) {
     document.getElementById('costsTxt').innerHTML = "<b>Total cost: " + roundTo(SeminarCosts,2).toFixed(2) + " €";
@@ -207,24 +222,28 @@ function displayCosts(SeminarCosts) {
 function convertListToMatrix() {
     let retArray = Array.from({ length: 5 }, () => new Array(4).fill(false));
     retArray[0][0] = inputList[0];
-    retArray[0][2] = inputList[7];
-    retArray[0][3] = inputList[11];
+    retArray[0][1] = inputList[5];
+    retArray[0][2] = inputList[10];
+    retArray[0][3] = inputList[15];
 
     retArray[1][0] = inputList[1];
-    retArray[1][2] = inputList[8];
-    retArray[1][3] = inputList[12];
+    retArray[1][1] = inputList[6];
+    retArray[1][2] = inputList[11];
+    retArray[1][3] = inputList[16];
 
     retArray[2][0] = inputList[2];
-    retArray[2][2] = inputList[9];
-    retArray[2][3] = inputList[13];
+    retArray[2][1] = inputList[7];
+    retArray[2][2] = inputList[12];
+    retArray[2][3] = inputList[17];
 
     retArray[3][0] = inputList[3];
-    retArray[3][1] = inputList[5];
-    retArray[3][2] = inputList[10];
-    retArray[3][3] = inputList[14];
+    retArray[3][1] = inputList[8];
+    retArray[3][2] = inputList[13];
+    retArray[3][3] = inputList[18];
 
     retArray[4][0] = inputList[4];
-    retArray[4][1] = inputList[6];
+    retArray[4][1] = inputList[9];
+    retArray[4][2] = inputList[14];
 
     return retArray;
 }
